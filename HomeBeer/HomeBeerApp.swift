8c35ed0e52c5f805a3d563d085520aa3ec51e9cd
Gian Ferreira <gianferreira@brq.com>
2023-10-09 19:35:43 -0300

    Initial Commit

---
+++ HomeBeer/HomeBeerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeBeerApp.swift
+//  HomeBeer
+//
+//  Created by user on 09/10/23.
+//
+
+import SwiftUI
+
+@main
+struct HomeBeerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
